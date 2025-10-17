366921bd523dfb8769e8a805c4927937e64ee47b
AntLS <73178474+AntLSt@users.noreply.github.com>
2025-10-17 17:27:06 +0100

    Added function for building Control Packets to application_layer.c; Added function for building Data Packets to application_layer.c; Added .gitignore file to project root;

--- project/src/application_layer.c
+++ project/src/application_layer.c
@@ -5,13 +5,25 @@
 
 #include <stdio.h>
 
+// Error Handling
+
 void errorExit(const char *error_message);
 
+// Transmitter
+
+#define DATA_PACKET_START 0x01
+#define DATA_PACKET_DATA 0x02
+#define DATA_PACKET_END 0x03
+
 int transmitter(const char *filename);
-int buildControlPacket(unsigned char *packet, unsigned int filesize, const char *filename);
-int buildDataPacket(unsigned char *packet, unsigned char *data_field, unsigned int data_size);
+int buildControlPacket(unsigned char *packet, const char control_field, unsigned int filesize, const char *filename);
+int buildDataPacket(unsigned char *packet, unsigned char *data_field, int data_size);
+
+// Receiver
 
 int receiver(const char *filename);
+
+// Application Layer main function
 
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
@@ -38,6 +50,9 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
     return;
 }
 
+// Error Handling function that Exits program with value 1 (EXIT_FAILURE)
+// Arguments:
+//  error_message: message to printf() before exiting the program.
 void errorExit(const char *error_message) {
     printf("\nCritical Error ocurred.\n");
     printf(error_message);
@@ -53,20 +68,66 @@ int transmitter(const char *filename) {
     return 1;
 }
 
-int buildControlPacket(unsigned char *packet, unsigned int filesize, const char *filename) {
+// Builder function for Control Packets
+// Arguments:
+//  packet: empty buffer to write control packet.
+//  control_field: octet of value 0x01 (START) or 0x03 (END)
+//  data_size: size of the data provided in the data_field for this data packet. Must range between 0 and min(65535, MAX_PAYLOAD_SIZE - 3).
+// Return 0 on success or -1 on error.
+int buildControlPacket(unsigned char *packet, const char control_field, unsigned int filesize, const char *filename) {
 
-    // TODO
-    printf("\"buildControlPacket()\" function not yet implemented.\n");
+    // Check if control_field is expected value
+    if (control_field != 0x01 || control_field != 0x03) {
+        printf("Unexpected value of control field for control packet. Must be either 0x01 (START) or 0x03 (END).\n");
+        return 1;
+    }
 
-    return 1;
+    // Control Packet Parameter positions
+    int filesize_octet = 1;
+    int filename_octet = 7;
+
+    // Check if size of filename is within expected values
+    if (sizeof(filename) > MAX_PAYLOAD_SIZE - filename_octet) {
+        printf("Size of Filename exceed limits.\n");
+        return 1;
+    }
+
+    // Control Field
+    strncpy(packet[0], control_field, 1);
+
+    // File Size parameter
+    strncpy(packet[filesize_octet + 0], 0x00, 1);
+    strncpy(packet[filesize_octet + 1], 0x04, 1);
+    strncpy(packet[filesize_octet + 2], filesize, 4);
+
+    // File Name parameter
+    strncpy(packet[filename_octet + 0], 0x00, 1);
+    strncpy(packet[filename_octet + 1], 0x04, 1);
+    strncpy(packet[filename_octet + 2], filename, sizeof(filename));
+
+    return 0;
 }
 
-int buildDataPacket(unsigned char *packet, unsigned char *data_field, unsigned int data_size) {
+// Builder Function for Data Packets
+// Arguments:
+//  packet: empty buffer to write data packet.
+//  data_field: location of data to write into the data field portion of the data packet.
+//  data_size: size of the data provided in the data_field for this data packet. Must range between 0 and min(65535, MAX_PAYLOAD_SIZE - 3).
+// Return 0 on success or -1 on error.
+int buildDataPacket(unsigned char *packet, unsigned char *data_field, int data_size) {
 
-    // TODO
-    printf("\"buildDataPacket()\" function not yet implemented.\n");
+    // Check if data_size is within expected values
+    if (data_size < 0 || data_size > 65535 || data_size > MAX_PAYLOAD_SIZE - 3) {
+        printf("Parameter \"data_size\" was outside expected range [0, min(65535, MAX_PAYLOAD_SIZE - 3)].\n");
+        return 1;
+    }
 
-    return 1;
+    // Control Field
+    strncpy(packet[0], DATA_PACKET_DATA, 1);
+    strncpy(packet[1], (unsigned short) data_size, 2);
+    strncpy(packet[3], data_field, data_size);
+
+    return 0;
 }
 
 int receiver(const char *filename) {
