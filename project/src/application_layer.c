337b833f4949950dd7de7e82fe9655d5fb1e91d2
AntLS <73178474+AntLSt@users.noreply.github.com>
2025-10-17 15:28:20 +0100

    application layer initial structure

--- project/src/application_layer.c
+++ project/src/application_layer.c
@@ -1,11 +1,39 @@
 // Application layer protocol implementation
 
 #include "application_layer.h"
+#include "link_layer.h"
 
 #include <stdio.h>
+
+int transmitter();
+
+int receiver();
 
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
-    // TODO: Implement this function
+    // Link layer connection parameters
+    LinkLayer connect_params = {serialPort, role, baudRate, nTries, timeout};
+
+    // open Data Link connection
+    if (llopen(connect_params) != 0) {
+        // TODO error handling
+    }
+
+    // Transmitter and Receiver have different processes
+    if (strcmp("tx", role) == 0) {
+        // TODO transmitter
+    } else
+    if (strcmp("rx", role) != 0) {
+        // TODO receiver
+    } else {
+        // TODO error handling
+    }
+
+    // close Data Link connection
+    if (llclose() != 0) {
+        // TODO error handling
+    }
+
+    return;
 }
