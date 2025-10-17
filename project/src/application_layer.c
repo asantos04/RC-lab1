d536ac8ee37e10a516d77abdf2b003131d492c76
AntLS <73178474+AntLSt@users.noreply.github.com>
2025-10-17 15:43:01 +0100

    Error handling;

--- project/src/application_layer.c
+++ project/src/application_layer.c
@@ -6,8 +6,8 @@
 #include <stdio.h>
 
 int transmitter();
-
 int receiver();
+void errorExit(const char *error_message);
 
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
@@ -16,24 +16,27 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
     LinkLayer connect_params = {serialPort, role, baudRate, nTries, timeout};
 
     // open Data Link connection
-    if (llopen(connect_params) != 0) {
-        // TODO error handling
-    }
+    if (llopen(connect_params) != 0) errorExit("Failed to open Data Link.\n");
 
     // Transmitter and Receiver have different processes
     if (strcmp("tx", role) == 0) {
         // TODO transmitter
-    } else
+    }
+    else
     if (strcmp("rx", role) != 0) {
         // TODO receiver
-    } else {
-        // TODO error handling
     }
+    else errorExit("Unexpected value for Data Link role.\nRole must be \"tx\" or \"rx\".\n");
 
     // close Data Link connection
-    if (llclose() != 0) {
-        // TODO error handling
-    }
+    if (llclose() != 0) errorExit("Failed to close Data Link.\n");
 
     return;
+}
+
+void errorExit(const char *error_message) {
+    printf("\nCritical Error ocurred.\n");
+    printf(error_message);
+    printf("Exiting program.\n");
+    exit(1);
 }
